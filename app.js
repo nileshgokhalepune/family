79a330c9680b878b72c3533dcb292257803ca77d
ICG\ngokhal <nilesh.gokhale@ingramcontent.com>
2017-11-06 13:20:41 -0600

    track env

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ if (!dev) {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+console.log(process.env);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
